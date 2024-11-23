5f33a105bfb8daacfcfaee27aaead913fd8c3964
chirbard <matamm@outlook.com>
2024-11-23 17:35:10 +0200

    feat: single threaded prompting on all laws

--- sort-data-into-groups/main.py
+++ sort-data-into-groups/main.py
@@ -8,5 +8,5 @@ output_folder = './output'
 
 file_sizes = get_file_sizes(data_folder=data_folder)
 # Note: the token limit should also fit the system prompt and user prompt
-groups = get_groups(file_sizes=file_sizes, group_token_limit=1000000)
+groups = get_groups(file_sizes=file_sizes, group_token_limit=998000)
 generate_new_files(groups=groups, data_folder=data_folder, output_folder=output_folder)
